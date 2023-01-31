bd7a5151c3bc0ce9e40bb58dcb9b8416df06e2c5
Nikita <nikita.babych@wearehathway.com>
2023-01-31 22:57:47 +0400

    Marketing page v1

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { RoutePaths } from "../../route-paths.enum";
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
-
+  public readonly ROUTE_PATHS = RoutePaths;
 }
