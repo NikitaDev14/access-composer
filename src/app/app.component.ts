c5c85b6a1b1f242f532538dc58f202d367c597e0
nikita <mykytababych1989@gmail.com>
2024-01-26 17:43:59 +0400

    Updated dependencies. Updated landing. Added headers to the demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
+import { filter, map, Observable } from "rxjs";
+
+import { RoutePaths } from "./route-paths.enum";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    protected readonly ROUTE_PATHS = RoutePaths;
+
+    public isDemoLinkShown$!: Observable<boolean>;
+
+    constructor(
+      private router: Router,
+    ) { }
+
+  ngOnInit() {
+      this.isDemoLinkShown$ = this.router.events.pipe(
+        filter((routerEvent: any) => routerEvent instanceof NavigationEnd),
+        map((routerEvent: NavigationEnd) => {
+          return !routerEvent.urlAfterRedirects.includes(RoutePaths.DEMO);
+        }),
+      );
+  }
+}
